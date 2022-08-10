6dcdcbdd87c14e0fcf9125b658a3acc4afaae816
zzq123 <1101921989@qq.com>
2022-08-10 21:33:23 +0800

    完善createElement方法

--- react15/src/ZzqReact/createElement.js
+++ react15/src/ZzqReact/createElement.js
@@ -1,7 +1,20 @@
 export default function createElement(type, props, ...children) {
+    const newChildren = [].concat(...children).reduce((acc, next) => {
+        if (next !== false && next !== true && next !== null) {
+            if (next instanceof Object) {
+                acc.push(next)
+            } else {
+                acc.push(createElement('text', {textContent: next}))
+            }
+        }
+        return acc
+    }, [])
+
     return {
         type,
-        props,
-        children
+        props: {
+            ...props,
+            children: newChildren
+        },
     }
 }
--- react15/src/index.js
+++ react15/src/index.js
@@ -1,5 +1,16 @@
+import React from 'react';
 import ZzqReact from './ZzqReact';
 
 console.log(ZzqReact);
 
-console.log(<div className="container">1</div>);
+console.log(<div className="container">
+    <div>
+        <p>这是橘子</p>
+        <button>这是按钮</button>
+        位置
+    </div>
+    {2 ===2 && <div>222</div>}
+    {2 === 1 && <div>111</div>}
+    <input type="text" value='123' />
+    这只是一个文字
+</div>);
